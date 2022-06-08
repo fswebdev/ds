1887357b9495bb5e659035b5e08aeb42d0ecebcf
fswebdev <47744494+fswebdev@users.noreply.github.com>
2022-06-08 22:08:26 +0530

    Update Weather.js

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -7,7 +7,7 @@ const CardExampleCard = ({weatherData}) => (
     <Card>
         <Card.Content>
             <Card.Header className='header'>City Name: {weatherData.name}</Card.Header>
-            <p>Temperature: {weatherData.main.temp}</p>
+            <p>Temperature: {(weatherData.main.temp * 1.8 + 32).toFixed(2)}</p>
             <p>Day: {moment().format('ddddd')}</p>
             <p>Date: {moment().format('LL')}</p>
         </Card.Content>
