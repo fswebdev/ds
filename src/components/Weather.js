7db22defa83902cd49b72aa45a541329377985cb
fswebdev <47744494+fswebdev@users.noreply.github.com>
2022-06-08 19:36:25 +0530

    Add files via upload

---
+++ src/components/Weather.js
@@ -0,0 +1,17 @@
+import React from 'react';
+//import './styles.css';
+import { Card } from 'semantic-ui-react';
+import moment from 'moment';
+
+const CardExampleCard = ({weatherData}) => (
+    <Card>
+        <Card.Content>
+            <Card.Header className='header'>City Name: {weatherData.name}</Card.Header>
+            <p>Temperature: {weatherData.main.temp}</p>
+            <p>Day: {moment().format('ddddd')}</p>
+            <p>Date: {moment().format('LL')}</p>
+        </Card.Content>
+    </Card>
+)
+
+export default CardExampleCard;
